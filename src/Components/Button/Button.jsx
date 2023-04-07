aea5271b82e7a355d645b02d54d388f7fc40acde
hungtech511 <vhung.webdev@gmail.com>
2023-04-07 08:37:30 +0700

    init list item component

---
+++ src/Components/Button/Button.jsx
@@ -0,0 +1,15 @@
+import { FontSizeTheme } from "@assets/theme"
+import "./Button.scss";
+
+const CustomButton = (color, hover) =>{
+    return(
+        <button className="btn-theme">
+            <span className="btn-wrap">
+                <span className="text-one">Get Started</span>
+                <span className="text-two">Get Started</span>
+            </span>
+            <i class="far fa-long-arrow-right"></i>
+        </button>
+    )
+}
+export default CustomButton;
