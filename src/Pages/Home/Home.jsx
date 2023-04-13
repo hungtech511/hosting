c1e564d4f83d27ca16ac441798bb38ca7abffb6c
hungtech511 <vhung.webdev@gmail.com>
2023-04-13 11:38:39 +0700

    fix merge

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,30 +4,11 @@ import HomeServices from "@layouts/Home/HomeServices";
 import CallUsNow from "@layouts/Home/CallUsNow";
 import SecurityFirst from "@layouts/Home/SecurityFirst";
 import LatestFeatures from "@layouts/Home/LatestFeatures";
-
-import "./Home.scss";
 import ControlPanel from "@layouts/Home/ControlPanel";
 
-const Home = () => {
-
-
-const renderBackground = () => {
-	return (
-		<>
-			{
-				listBackgroundHomeImage.map((item, index) => {
-					return (
-						<ItemBackground key={item?.id} className={`icon-layer ${item?.className}`} imageSrc={item?.imageSrc} />
-					)
-				})
-			}
-		</>
-	)
-}
+import "./Home.scss";
 
 const Home = () => {
-  const fonts = FontSizeTheme()
-  const colors = ColorTheme()
 	return (
 		<>
 			<HomeBackground background="backgroundHome"/>
